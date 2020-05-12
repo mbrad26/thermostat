46b75ec534134d309f7dd670043c6d2c6149ea14
mbrad26 <mbrad26@gmail.com>
2020-05-12 18:40:03 +0100

    implemented PSM

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -16,4 +16,37 @@ describe('Thermostat', function () {
 
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
+
+  it('can turn the temperature down', function () {
+    thermostat.down();
+
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10 degrees', function () {
+    for (let i = 0; i < 10; i++) {
+      thermostat.down();
+    }
+
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has power saving mode ON by default', function () {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('can turn power saving mode OFF', function () {
+    thermostat.switchPowerSavingModeOff();
+
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('can turn power saving mode ON', function () {
+    thermostat.switchPowerSavingModeOff();
+
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,9 @@
-'use strict'
+'use strict';
 
 class Thermostat {
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
+    this.powerSavingMode = true;
     this.temperature = 20;
   }
 
@@ -11,5 +13,29 @@ class Thermostat {
 
   up() {
     this.temperature += 1;
+  }
+
+  down() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
+
+    this.temperature -= 1;
+  }
+
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
   }
 }
