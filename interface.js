ede2eeb1ffde0888cec0fbb7ca1489784b7df849
mbrad26 <mbrad26@gmail.com>
2020-05-14 13:55:59 +0100

    implemented openweather api

--- interface.js
+++ interface.js
@@ -27,8 +27,25 @@ $(document).ready(function () {
     $('#power-saving-status').text('OFF');
   });
 
+  displayWeather('London');
+
+  $('#select-city').submit(function (event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function (data) {
+      $('#current-temperature').text(data.main.temp);
+    });
   }
 });
