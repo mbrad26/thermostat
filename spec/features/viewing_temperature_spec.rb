ec8cbec2e3696fc92d8c08a2e73a66477c6453b4
Joanne Young <joanneyoung_@hotmail.com>
2020-05-15 15:16:37 +0100

    user tests for increase and decrease added

--- spec/features/viewing_temperature_spec.rb
+++ spec/features/viewing_temperature_spec.rb
@@ -1,70 +1,43 @@
 feature 'Viewing the temperature settings' do
-  scenario 'a user can see the thermostat settings and temperature' do
-    visit '/'
 
-    expect(page).to have_content "Thermostat"
-    expect(page).to have_button '+'
-    expect(page).to have_button '-'
-    expect(page).to have_button 'reset'
-    expect(page).to have_button 'PSM On'
-    expect(page).to have_button 'PSM Off'
+  scenario 'viewing temperature button and settings' do
+    visit('/')
+    expect(page).to have_button('+')
+    expect(page).to have_button('-')
+    expect(page).to have_button('reset')
+    expect(page).to have_button('PSM On')
+    expect(page).to have_button('PSM Off')
+    expect(page).to have_css('#temperature')
+    expect(page).to have_css('#power-saving-status')
   end
 
-  scenario 'a user can ajust the temperature up' do
-    visit '/'
-
-    expect(page.find_by_id('temperature')).to have_content '20'
-
-    3.times do
-      click_button '+'
+  scenario 'user can see the temperature increase' do
+    visit('/')
+    expect(page).to have_content('20')
+    3.times do 
+      click_button('+')
     end
-
-    expect(page.find_by_id('temperature')).not_to have_content '20'
-    expect(page.find_by_id('temperature')).to have_content '23'
+    expect(page).to have_content('23')
   end
 
-  scenario 'a user can ajust the temperature down' do
-    visit '/'
-
-    expect(page.find_by_id('temperature')).to have_content '20'
-
-    3.times do
-      click_button '-'
+  scenario 'user can see the temperature decrease' do
+    visit('/')
+    expect(page).to have_content('20')
+    3.times do 
+      click_button('-')
     end
-
-    expect(page.find_by_id('temperature')).not_to have_content '20'
-    expect(page.find_by_id('temperature')).to have_content '17'
+    expect(page).to have_content('17')
   end
 
-  scenario 'a user can reset the temperature to default' do
-    visit '/'
 
-    expect(page.find_by_id('temperature')).to have_content '20'
+  # scenario 'user can increase to 32 on power saving mode off' do
+  #   visit('/')
+  #   expect(page).to have_content('20')
+  #   click_button('PSM Off')
+  #   12.times do 
+  #     click_button('+')
+  #   end
 
-    5.times do
-      click_button '+'
-    end
+  # end
 
-    expect(page.find_by_id('temperature')).to have_content '25'
-
-    click_button 'reset'
-
-    expect(page.find_by_id('temperature')).to have_content '20'
-  end
-
-  scenario 'power saving mode is ON by default' do
-    visit '/'
-
-    expect(page.find_by_id('power-saving-status')).to have_content 'ON'
-  end
-
-  scenario 'power saving mode can be turned OFF' do
-    visit '/'
-
-    expect(page.find_by_id('power-saving-status')).to have_content 'ON'
-
-    click_button 'PSM Off'
-
-    expect(page.find_by_id('power-saving-status')).to have_content 'OFF'
-  end
 end
