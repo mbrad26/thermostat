e21abccc28d48b05b44f60f12f80d56bd2a88ae7
mbrad26 <mbrad26@gmail.com>
2020-05-15 13:59:18 +0100

    added feature tests for PSM ON and OFF

--- spec/features/viewing_temperature_spec.rb
+++ spec/features/viewing_temperature_spec.rb
@@ -10,32 +10,61 @@ feature 'Viewing the temperature settings' do
     expect(page).to have_button 'PSM Off'
   end
 
-  scenario 'a user can ajust the temperature up and down' do
+  scenario 'a user can ajust the temperature up' do
     visit '/'
 
-    expect(page.find_by_id('temperature').text).to eq '20'
+    expect(page.find_by_id('temperature')).to have_content '20'
 
-    click_button '+'
+    3.times do
+      click_button '+'
+    end
 
-    expect(page.find_by_id('temperature').text).not_to eq '20'
-    expect(page.find_by_id('temperature').text).to eq '21'
-
-    click_button '-'
-    click_button '-'
-
-    expect(page.find_by_id('temperature').text).not_to eq '21'
-    expect(page.find_by_id('temperature').text).to eq '19'
+    expect(page.find_by_id('temperature')).not_to have_content '20'
+    expect(page.find_by_id('temperature')).to have_content '23'
   end
 
-  # scenario 'a user can ajust the temperature down' do
-  #   '/visit'
-  #
-  #   expect(page.find_by_id('temperature').text).to eq '20'
-  #
-  #   click_button '-'
-  #   click_button '-'
-  #
-  #   expect(page.find_by_id('temperature').text).not_to eq '20'
-  #   expect(page.find_by_id('temperature').text).to eq '18'
-  # end
+  scenario 'a user can ajust the temperature down' do
+    visit '/'
+
+    expect(page.find_by_id('temperature')).to have_content '20'
+
+    3.times do
+      click_button '-'
+    end
+
+    expect(page.find_by_id('temperature')).not_to have_content '20'
+    expect(page.find_by_id('temperature')).to have_content '17'
+  end
+
+  scenario 'a user can reset the temperature to default' do
+    visit '/'
+
+    expect(page.find_by_id('temperature')).to have_content '20'
+
+    5.times do
+      click_button '+'
+    end
+
+    expect(page.find_by_id('temperature')).to have_content '25'
+
+    click_button 'reset'
+
+    expect(page.find_by_id('temperature')).to have_content '20'
+  end
+
+  scenario 'power saving mode is ON by default' do
+    visit '/'
+
+    expect(page.find_by_id('power-saving-status')).to have_content 'ON'
+  end
+
+  scenario 'power saving mode can be turned OFF' do
+    visit '/'
+
+    expect(page.find_by_id('power-saving-status')).to have_content 'ON'
+
+    click_button 'PSM Off'
+
+    expect(page.find_by_id('power-saving-status')).to have_content 'OFF'
+  end
 end
